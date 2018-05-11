6843851ce89fdc79bba302a9d05194d4c673213e
meng  zhou <mengzhou44@mengs-MacBook-Pro.local>
2018-05-10 22:37:51 -0600

    no comment

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../_common/header';
 
 export default (props) => {
     return (
         <div>
+            <Header />
             <h1> Home Page </h1>
         </div>
     );
