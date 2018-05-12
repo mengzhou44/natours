69748d6a3b2b87d0800a0b3434741739a6f6a3cc
meng  zhou <mengzhou44@mengs-MacBook-Pro.local>
2018-05-11 22:08:06 -0600

    no comment

--- src/pages/_common/story.js
+++ src/pages/_common/story.js
@@ -4,16 +4,18 @@ import React from 'react';
 export default (props) => {
 
     return (
-        <div className='story'>
-            <figure className='story__shape'>
-                <img src={props.imageSource} alt='story image' className='story__img' />
-                <figcaption className='story__caption'>{props.imageCaption}</figcaption>
-            </figure>
-            <div className='story__text'>
-                <h3 className='heading-tertiary margin-bottom-small'>{props.title}</h3>
-                <p>
-                    {props.content}
-                </p>
+        <div className='row'>
+            <div className='story'>
+                <figure className='story__shape'>
+                    <img src={props.imageSource} alt='story image' className='story__img' />
+                    <figcaption className='story__caption'>{props.imageCaption}</figcaption>
+                </figure>
+                <div className='story__text'>
+                    <h3 className='heading-tertiary margin-bottom-small'>{props.title}</h3>
+                    <p>
+                        {props.content}
+                    </p>
+                </div>
             </div>
         </div>
     );
--- src/pages/home/stories.js
+++ src/pages/home/stories.js
@@ -10,16 +10,32 @@ export default (props) => {
                </h2>
             </div>
 
-            <div className='row'>
-                <Story
-                    imageSource='img/nat-9.jpg'
-                    imageCaption='Jack Wilson'
-                    title='WOW! My life is completely different now'
-                    content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur
-                    ducimus quam nisi exercitationem omnis earum qui.Aperiam, ipsum sapiente aspernatur libero
-                      repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui."
+
+            <div class="bg-video">
+                <video class="bg-video__content" autoPlay muted loop>
+                    <source src="img/video.mp4" type="video/mp4" />
+                    <source src="img/video.webm" type="video/webm" />
+                    Your browser does not support Videos!
+                </video>
+            </div>
+
+            <Story
+                imageSource='img/nat-8.jpg'
+                imageCaption='Mary Smith'
+                title='I had the best week ever with my family'
+                content='   Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur
+                ducimus quam nisi exercitationem omnis earum qui.Aperiam, ipsum sapiente aspernatur libero
+                    repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui.'
                 />
-             </div>
+
+                <Story
+                imageSource='img/nat-9.jpg'
+                imageCaption='Jack Wilson'
+                title='WOW! My life is completely different now'
+                content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur
+                ducimus quam nisi exercitationem omnis earum qui.Aperiam, ipsum sapiente aspernatur libero
+                      repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui.'
+                />
 
         </section>
 
